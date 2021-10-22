042ae3f867dfd4f4a13c9e07c6fe28de9662adc9
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@jointacademy.com>
2021-10-22 15:45:37 +0300

    Refactor

--- src/image_formats/webp.nim
+++ src/image_formats/webp.nim
@@ -1,9 +1,58 @@
-import std/[streams,bitops,strutils,endians]
+import std/[streams,bitops,endians]
 
 
-proc get_size_from_vp8_header_block( stream: var FileStream ): (int,int)
-proc get_size_from_vp8l_header_block( stream: var FileStream ): (int,int)
-proc get_size_from_vp8x_header_block( stream: var FileStream ): (int,int)
+type
+  Errors* = enum
+    Invalid = (1, "File is not a valid Webp")
+
+
+
+# Helper to read 4 byte integers in little endian.
+proc read_int32( stream: FileStream ): int =
+  var raw = stream.read_uint32()
+  littleEndian32( result.addr, raw.addr)
+
+
+# Lossy - Size is two bytes, 14 bits length and 2 bits scale, one based starting
+# at byte 26.
+proc get_size_from_vp8_header_block( stream: var FileStream ): (int,int) =
+  stream.set_position( 26 )
+  let raw = stream.read_int32()
+  let width = int( raw.bitsliced( 0 .. 13 ))
+  let height = int( raw.bitsliced( 16 .. 29 ))
+
+  return (width, height)
+
+
+
+# Lossless - Size is two 14 bit values, back to back, starting at byte 21.
+# Size is also zero based, actual size - 1, so we need to add one.
+proc get_size_from_vp8l_header_block( stream: var FileStream ): (int,int) =
+  stream.set_position( 21 )
+  let raw = stream.read_int32()
+  let width = int( raw.bitsliced( 0 .. 13 )) + 1
+  let height = int( raw.bitsliced( 14 .. 27 )) + 1
+
+  return (width, height)
+
+
+
+# Extended - Size is two 3 byte integers starting at byte 24 resp. 27
+# Size is also zero based, actual size - 1, so we need to add one.
+proc get_size_from_vp8x_header_block( stream: var FileStream ): (int,int) =
+    var raw:int
+
+    stream.set_position( 24 )
+    raw = stream.read_int32()
+    let width = int( raw.bitsliced( 0 .. 23 )) + 1
+
+    stream.set_position( 27 )
+    raw = stream.read_int32()
+    let height = int( raw.bitsliced( 0 .. 23 )) + 1
+
+    return ( width, height )
+
+
 
 proc get_size*( path:string ): (int,int) =
   var stream = new_file_stream( path, fm_read )
@@ -19,58 +68,7 @@ proc get_size*( path:string ): (int,int) =
     of "VP8X":
       get_size_from_vp8x_header_block( stream )
     else:
-      echo "Unknown Webp encoding: " & encoding
       return (-1,1)
-
-
-
-proc read_basic_size_values_at_position( stream: var FileStream, pos: int ): uint32 =
-  var buffer: array[4, byte]
-  var section:uint32
-
-  stream.set_position(pos)
-  doAssert stream.readData(buffer.addr, 4) == 4
-  section = cast[uint32](buffer)
-
-  littleEndian32( result.addr, section.addr)
-
-proc get_size_from_vp8_header_block( stream: var FileStream ): (int,int) =
-  let raw = stream.read_basic_size_values_at_position(26)
-  let w = raw.bitsliced(0 .. 13)
-  let h = raw.bitsliced(16 .. 29)
-
-  return (int(w),int(h))
-
-
-
-proc get_size_from_vp8l_header_block( stream: var FileStream ): (int,int) =
-  let raw = stream.read_basic_size_values_at_position(21)
-  let w = raw.bitsliced(0 .. 13)
-  let h = raw.bitsliced(14 .. 27)
-
-  return (int(w)+1,int(h)+1)
-
-
-
-proc read_extended_size_value_at_position( stream: var FileStream, pos: int ): int =
-  var buffer: array[4, byte]
-  var section:uint32
-  var raw:uint32
-
-  stream.set_position(24)
-  doAssert stream.readData(buffer.addr, 4) == 4
-  section = cast[uint32](buffer)
-
-  littleEndian32( raw.addr, section.addr)
-
-  int(raw.bitsliced(0 .. 23))
-
-
-proc get_size_from_vp8x_header_block( stream: var FileStream ): (int,int) =
-    let w = stream.read_extended_size_value_at_position( 24 )
-    let h = stream.read_extended_size_value_at_position( 27 )
-
-    return (int(w)+1,int(h)+1)
 
 
 
