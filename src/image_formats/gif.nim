202251c5d30b49ac9a7a2e585ea4dbf0aef362cc
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@jointacademy.com>
2021-10-22 12:52:44 +0300

    Refactoring

--- src/image_formats/gif.nim
+++ src/image_formats/gif.nim
@@ -8,32 +8,28 @@
 
 import std/[streams, endians]
 
+type
+  Errors* = enum
+    Invalid = (1, "File is not a valid Gif")
+
+proc read_int16( stream:FileStream ): int =
+  var raw = stream.readUInt16()
+  littleEndian16( result.addr, raw.addr)
+
 proc get_size*( path: string ): (int,int) =
   var stream = new_file_stream( path, fm_read )
   defer: stream.close()
 
-  stream.setPosition(0)
   onFailedAssert(msg):
     return (-1,1)
 
-  doAssert stream.readChar() == 'G'
-  doAssert stream.readChar() == 'I'
-  doAssert stream.readChar() == 'F'
-  doAssert stream.readChar() == '8'
-  doAssert stream.readChar() == '7' or stream.readChar() == '9'
-  doAssert stream.readChar() == 'a'
+  let magic_bytes = stream.readStr(6)
+  doAssert magic_bytes == "GIF87a" or magic_bytes == "GIF89a"
 
-  var section:uint16
-  var raw:uint16
+  let width = stream.read_int16()
+  let height = stream.read_int16()
 
-  section = stream.readUInt16()
-  littleEndian16( raw.addr, section.addr)
-  let w = int(raw)
-  section = stream.readUInt16()
-  littleEndian16( raw.addr, section.addr)
-  let h = int(raw)
-
-  return (w,h)
+  return ( width, height )
 
 assert get_size( "src/image_formats/examples/jonas.gif" ) == (512,512)
 assert get_size( "src/image_formats/examples/jonas.jpg" ) == (-1,1)
